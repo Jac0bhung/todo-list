c35c4653287a0a844ed116bc90499b96cb635bbe
Jac0bhung <vasortgrav@gmail.com>
2022-12-13 21:43:02 +0800

    更新app.js

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  // res.send("hello world");
   res.render("index");
 });
 
