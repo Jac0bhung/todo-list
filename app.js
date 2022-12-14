a25a842df125e84aa4bbb9cd03f44d9a2dd6df54
Jac0bhung <vasortgrav@gmail.com>
2022-12-14 16:22:44 +0800

    feat:add create todo funtion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const Todo = require("./models/todo");
 
 if (process.env.NODE_ENV !== "production") {
@@ -21,8 +22,20 @@ db.once("open", () => {
   console.log("mongodb connected!");
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+
+app.get("/todos/new", (req, res) => {
+  return res.render("new");
+});
+
+app.post("/todos", (req, res) => {
+  const name = req.body.name;
+  return Todo.create({ name })
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
 app.get("/", (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
